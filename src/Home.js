2b249a00b2d58f7683be376d50e13b29e4c786d2
Mayte Velueta <maytevelueta@gmail.com>
2023-01-12 18:31:10 -0600

    120123

---
+++ src/Home.js
@@ -0,0 +1,9 @@
+const Home = () => {
+    return ( 
+        <div className="home">
+            <h3>Welcome to my virtual gallery</h3>
+        </div>
+     );
+}
+ 
+export default Home ;
---
+++ src/Navbar.js
@@ -0,0 +1,14 @@
+const Navbar = () => {
+    return ( 
+        <nav className="navbar">
+            <h1>marove aqua</h1>
+            <div className="links">
+                <a href="/">Home</a>
+                <a href="/">Gallery</a>
+                <a href="/">Contact</a>
+            </div>
+        </nav>
+     );
+}
+ 
+export default Navbar;
