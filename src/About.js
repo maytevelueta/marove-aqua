d677a278c8acc69d924bfb56aa1e2c194be38f32
Mayte Velueta <maytevelueta@gmail.com>
2023-01-24 10:04:26 -0600

    2401

---
+++ src/About.js
@@ -0,0 +1,11 @@
+import React from "react";
+
+const About = () => {
+    return ( 
+        <div className="about">
+            <p>About me</p>
+        </div>
+     );
+}
+ 
+export default About;
---
+++ src/Contact.js
@@ -0,0 +1,15 @@
+import React from "react";
+import ContactSection from "./components/ContactSection";
+
+const Contact = () => {
+    return ( 
+        <div className="contact">
+            <ContactSection />
+        </div>
+     );
+}
+ 
+export default Contact;
+<div>
+
+</div>
