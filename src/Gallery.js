0e1f2e3432a668d948c7db183c5873390003d319
Mayte Velueta <maytevelueta@gmail.com>
2023-01-24 10:04:17 -0600

    2401

---
+++ src/Gallery.js
@@ -0,0 +1,12 @@
+import React from "react";
+import GalleryList from "./components/GalleryList";
+
+const Gallery = () => {
+    return ( 
+        <div className="gallery">
+            <GalleryList />
+        </div>
+     );
+}
+ 
+export default Gallery;
--- src/Navbar.js
+++ src/Navbar.js
@@ -1,18 +1,22 @@
+import './Navbar.css';
 
 const Navbar = () => {
     return ( 
+        <div className='navbar-container'>
         <nav className="navbar">
             <a href="/" className="logo">
             <img src="./assets/logo.png" alt="logo" style={{
                 width: "150px"
             }}/>
             </a>
-            <div className="links">
+            <div className="nav-menu">
                 <a href="/">Home</a>
-                <a href="/">Gallery</a>
+                <a href="/about">About</a>
+                <a href="/gallery">Gallery</a>
                 <a href="/">Contact</a>
             </div>
         </nav>
+        </div>
      );
 }
  
