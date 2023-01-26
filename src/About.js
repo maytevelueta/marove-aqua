43353d5035d7ceb4f97780a8904719f131dc2dd2
Mayte Velueta <maytevelueta@gmail.com>
2023-01-26 15:06:09 -0600

    2601

--- src/About.js
+++ src/About.js
@@ -1,12 +1,18 @@
+import tetraFull from "./assets/tetra-full.png";
 import React from "react";
 
 const About = () => {
     return ( 
-        <div className="about">
+        <div className="about-container">
             <h1>About me</h1>
-            <p style={{ fontSize: "120%" }}>I'm Mayté from México, and I'm a watercolor enthusiast. Marove Aqua started in early 2021 as a little personal side project to decorate my appartment. Before that, I had never painted with watercolors, and the experience was a game changer to me.</p>
+            <div className="about">
+            <img src={ tetraFull } alt="tetra-full" style={{ width: "40%" }}/>
+            <div className="about-text" style={{ fontSize: "120%", alignSelf: "center", paddingLeft: "1rem" }}>
+            <p>I'm Mayté from México, and I'm a watercolor enthusiast. Marove Aqua started in early 2021 as a little personal side project to decorate my appartment. Before that, I had never painted with watercolors, and the experience was a game changer to me.</p>
             <br/>
-            <p style={{ fontSize: "120%" }}>My paints are focused on hyperrealism still lifes, all made with Winsor and Newton pigments over cold pressed paper. I keep practicing and improving my techniques, and always looking for inspiration. If you have any suggestions or want to order a customize painting, please contact me and I'll be happy to reach you.</p>
+            <p>My paints are focused on hyperrealism still lifes, all made with Winsor and Newton pigments over cold pressed paper. I keep practicing and improving my techniques, and always looking for inspiration. If you have any suggestions or want to order a customize painting, please contact me and I'll be happy to reach you.</p>
+            </div>
+            </div>
         </div>
      );
 }
--- src/components/GalleryList.js
+++ src/components/GalleryList.js
@@ -1,10 +1,36 @@
-import firstImage from '../assets/kingfisher.png'
+import kingfisher from '../assets/kingfisher.png';
+import cardinal from '../assets/cardinal.png';
+import seahorse from '../assets/seahorse.png';
+import toh from '../assets/toh.png';
+import turtle from '../assets/turtle.png';
+import axolotl from '../assets/axolotl.png';
+import titWarbler from '../assets/tit-warbler.png';
+import { useState } from 'react';
 
 const GalleryList = () => {
+
+    const [galleryImages] = useState([
+        { title: "Kingfisher", photo: [kingfisher], body: "", author: "mayte", id: 1 },
+        { title: "Cardinal", photo: [cardinal], body: "", author: "mayte", id: 2 },
+        { title: "Seahorse", photo: [seahorse], body: "", author: "mayte", id: 3 },
+        { title: "Toh", photo: [toh], body: "", author: "mayte", id: 4 },
+        { title: "Turtle", photo: [turtle], body: "", author: "mayte", id: 5 },
+        { title: "Axolotl", photo: [axolotl], body: "", author: "mayte", id: 6 },
+        { title: "TitWarbler", photo: [titWarbler], body: "", author: "mayte", id: 6 }
+    ]);
+
+
     return ( 
-        <section>
-            <img src={firstImage} alt="kingfisher" />
-        </section>
+        <div>
+            <h1>Gallery</h1>
+            <div className="gallery-container">
+            {galleryImages.map((galleryImage) => (
+                <div className="gallery-card" key={ galleryImage.id }>
+                    <img className="gallery-image"  src={ galleryImage.photo } alt="projectimg"/>
+                </div>
+            ))}
+            </div>
+        </div>
      );
 }
  
