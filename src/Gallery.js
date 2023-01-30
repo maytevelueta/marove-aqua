9483d6b7593b2f56afda72521defcf552afa56dc
Mayte Velueta <maytevelueta@gmail.com>
2023-01-30 11:01:28 -0600

    3001

--- src/Gallery.js
+++ src/Gallery.js
@@ -5,6 +5,11 @@ import toh from './assets/toh.png';
 import turtle from './assets/turtle.png';
 import axolotl from './assets/axolotl.png';
 import titWarbler from './assets/tit-warbler.png';
+import shell from './assets/shell.png';
+import octopus from './assets/octopus.png';
+import butterfly from './assets/butterfly.png';
+import bouquet from './assets/bouquet.png';
+import amethyst from './assets/amethyst.png';
 import React from "react";
 import GalleryList from "./components/GalleryList";
 
@@ -17,7 +22,12 @@ function Gallery() {
             { title: "Toh", photo: [toh], body: "", author: "mayte", id: 4 },
             { title: "Turtle", photo: [turtle], body: "", author: "mayte", id: 5 },
             { title: "Axolotl", photo: [axolotl], body: "", author: "mayte", id: 6 },
-            { title: "TitWarbler", photo: [titWarbler], body: "", author: "mayte", id: 6 }
+            { title: "TitWarbler", photo: [titWarbler], body: "", author: "mayte", id: 7 },
+            { title: "Toh", photo: [shell], body: "", author: "mayte", id: 8 },
+            { title: "Toh", photo: [octopus], body: "", author: "mayte", id: 9 },
+            { title: "Toh", photo: [butterfly], body: "", author: "mayte", id: 10 },
+            { title: "Toh", photo: [bouquet], body: "", author: "mayte", id: 11 },
+            { title: "Toh", photo: [amethyst], body: "", author: "mayte", id: 12 }
         ];
 
     return ( 
