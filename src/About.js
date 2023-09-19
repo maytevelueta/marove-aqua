2a2de66a6c030499c25e12301be7676b3ea6c4ae
Mayte Velueta <maytevelueta@gmail.com>
2023-09-19 17:05:40 +0100

    1909

--- src/About.js
+++ src/About.js
@@ -8,7 +8,7 @@ const About = () => {
             <div className="about">
             <img src={ tetraFull } alt="tetra-full"/>
             <div className="about-text" style={{ fontSize: "120%", alignSelf: "center", paddingLeft: "1rem" }}>
-            <p>I'm Mayté from México, and I'm a watercolor enthusiast. Marove Aqua started in early 2021 as a little personal side project to decorate my appartment. Before that, I had never painted with watercolors, and the experience was a game changer to me.</p>
+            <p>I'm Mayte from Mexico, and I'm a watercolor enthusiast. Marove Aqua started in early 2021 as a little personal side project to decorate my appartment. Before that, I had never painted with watercolors, and the experience was a game changer to me.</p>
             <br/>
             <p>My paints are focused on hyperrealism still lifes, all made with Winsor and Newton pigments over cold pressed paper. I keep practicing and improving my techniques, and always looking for inspiration. If you have any suggestions or want to order a customize painting, please contact me and I'll be happy to reach you.</p>
             </div>
